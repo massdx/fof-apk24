6c64cca7f26296f5cdce972d564399c89e807af1
massdx <massahoudouodanou@gmail.com>
2023-12-02 09:45:48 +0000

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,35 @@ import LenisScroll from './lenis'
 import LoadingPage from '@/components/loading-page'
 const work = Work_Sans({ subsets: ['latin'], weight: ['400', '500' , '600' , '700' , '800'] })
 
-export const metadata: Metadata = {
-  title: 'APK 24',
-  description: 'description',
+export const metadata = {
+  openGraph: {
+    title: 'Africa product keynote : SEO title ',
+    description: "SEO decription ",
+    type: 'article',
+    publishedTime: '2023-8-31T00:00:00.000Z',
+    authors: ['TDEV COMMUNITY'],
+    url: 'festival.ourtdev.com',
+    siteName: 'Tdev festival 2023',
+    locale: 'fr_TG',
+    images: [
+      {
+        url: 'https://media.licdn.com/dms/image/D4D22AQFXza8Pa_TsnA/feedshare-shrink_800/0/1699878730955?e=1704326400&v=beta&t=PYmpFaM9BztvRRSY4Ew24f0GB4R-ys470HLHGH5BQNs',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://media.licdn.com/dms/image/D4D22AQFXza8Pa_TsnA/feedshare-shrink_800/0/1699878730955?e=1704326400&v=beta&t=PYmpFaM9BztvRRSY4Ew24f0GB4R-ys470HLHGH5BQNs',
+        width: 1800,
+        height: 1600,
+        alt: 'Africa product keynote',
+      },
+    ],
+  },
+  title: {
+    default: "APK Title SEO ici ",
+    template: "%s"
+  },
+  description: "description APK SEO "
 }
 
 export default function RootLayout({
